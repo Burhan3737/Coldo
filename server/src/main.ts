0b37b9b8ce137cf792561e55d0f2b62e630c2fdd
Burhan <burhan123.mb@gmail.com>
2024-08-21 16:08:03 +0500

    -Cos handling on backend

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173', // Replace with the URL of your frontend application
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true, // Allow credentials (e.g., cookies)
+  });
+
   await app.listen(3000);
 }
 bootstrap();
